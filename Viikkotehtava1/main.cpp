c3f32a24e336f88b1c47993b814ef8dd29c8279f
Elina Hertteli <c1heel00@students.oamk.fi>
2026-01-09 10:43:29 +0200

    viikkotehtava 1 Qt Creatorilla

---
+++ Viikkotehtava1/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int game(int maxnum);
+
+int main(){
+ srand(time(0));
+
+ int yritykset = game(40);
+ cout << "yritykset: " << yritykset; //tulostaa yritysten määrän
+
+ return 0;
+
+}
+
+int game(int maxnum){
+    int arvaus;
+    int etsittavaLuku;
+    int laskuri = 0;
+
+    etsittavaLuku = rand()% maxnum + 1; // %laskee jakojäännöksen, +1 avulla väli on 1-maxnum
+    //cout<<"random arvo on [0, " << maxnum <<"]: " << std::rand()<<'\n';
+    cout << "Arvaa luku 1-" << maxnum << endl; //tulostaa
+
+    do{
+        cout << "Anna arvaus: " << endl;
+        cin >> arvaus; //lukee arvauksen
+        laskuri++; //lisää arvausten määrää
+
+        if (arvaus == etsittavaLuku){
+            cout << "oikein" << endl;
+        }
+        else if (arvaus < etsittavaLuku){
+            cout << "liian pieni" << endl;
+        }
+        else{
+            cout << "liian suuri" << endl;
+            }
+        }while (arvaus != etsittavaLuku);
+
+    return laskuri;
+}
+
+
